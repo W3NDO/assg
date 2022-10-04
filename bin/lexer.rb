3afef75d715307f09a2bc04e4a9e1b5346f96363
W3NDO <wendonyang@gmail.com>
2022-10-04 06:23:30 +0300

    feat(develop/lexer)
    > Started working on the lexer by defining the subset of markdown grammar that I shall be tokenizing.
    > Added the RubyTree gem to be used to build the abstract syntax tree.

---
+++ bin/lexer.rb
@@ -0,0 +1,41 @@
+# code written by W3NDO
+# The lexer/tokenizer will split the markdown files into tokens
+
+=begin
+    We're trying to tokenize only a subset of markdown,
+    Specifically:
+        level 1 to level 4 titles (#, ##, ###, ####)
+        plain text
+        links { [text](link) }
+        links that become images { [img alt text](url/filepath) }
+        unordered list items ( hyphenated )
+        ordered list items ( numbers/alphabet )
+        styling tags (asterisks, underscores)
+    
+=end
+
+require 'tree' # https://rubygems.org/gems/rubytree
+
+TOKENS = {
+    :T_LEVEL_1_HEADER => /#/,
+    :T_LEVEL_2_HEADER => /##/,
+    :T_LEVEL_3_HEADER => /###/,
+    :T_LEVEL_4_HEADER => /####/,
+    :T_LEFT_SQUARE_BRACKET => /\]/,
+    :T_RIGHT_SQUARE_BRACKET => /\[/,
+    :T_LEFT_PAREN => /\(/,
+    :T_RIGHT_PAREN => /\)\/,
+    :T_HYPHEN => /\-/,
+    :T_UNDERSCORE => /\_/,
+    :T_DIGITS => /[0-9]/,
+    :T_ALPHABET => /[a-zA-Z]/,
+    :T_ASTERISK => /\*/
+}
+
+class Lexer
+    attr_accessor :ast_root
+
+    def initialize
+        self.ast_root = Tree::TreeNode.new("ROOT", "HTML")
+    end
+end
